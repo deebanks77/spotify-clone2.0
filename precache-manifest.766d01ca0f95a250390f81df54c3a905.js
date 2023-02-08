5e532dd1ed6b376f73b7b276129507632e6159ad
Banky <mac@macs-MacBook-Pro.local>
2023-02-08 15:20:46 +0100

    Updates

---
+++ precache-manifest.766d01ca0f95a250390f81df54c3a905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24e9823ca69e1624f2dd",
+    "url": "/spotify-clone2.0/static/css/main.2473fcd2.chunk.css"
+  },
+  {
+    "revision": "24e9823ca69e1624f2dd",
+    "url": "/spotify-clone2.0/static/js/main.bc80017e.chunk.js"
+  },
+  {
+    "revision": "ec8a7a93b3393dc510b5",
+    "url": "/spotify-clone2.0/static/js/runtime~main.803f8f38.js"
+  },
+  {
+    "revision": "449ba072be29ea2d11bd",
+    "url": "/spotify-clone2.0/static/js/2.5908a57d.chunk.js"
+  },
+  {
+    "revision": "32d45e1028e0956c33bc11201447546a",
+    "url": "/spotify-clone2.0/index.html"
+  }
+];
